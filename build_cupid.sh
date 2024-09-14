5bb68c846134b4d1951d265bdba2a49b29975971
carlyle-felix <carlyle.felix@gmail.com>
2024-09-14 08:11:07 +0200

    rename kernel_dir common

--- build_cupid.sh
+++ build_cupid.sh
@@ -2,7 +2,8 @@
 
 export TARGET_BOARD_PLATFORM=cupid
 export TARGET_BUILD_VARIANT=user
+export KERNEL_DIR=common
 export OUT_DIR=${ANDROID_BUILD_TOP}
 export LTO=thin
 
-BUILD_CONFIG=sm8450/build.config.cupid build/build.sh "$@"
+BUILD_CONFIG=common/build.config.cupid build/build.sh "$@"
